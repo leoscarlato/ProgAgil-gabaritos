76df1aaf1d1f8b35c2ba88c8d890c354de31eb1f
EduardoMVAz <emendesvaz@gmail.com>
2023-10-23 21:51:54 -0300

    código pronto, sem testar

---
+++ atividade_5/credentials_file.py
@@ -0,0 +1,10 @@
+settings = {
+    "host": "cluster-teste.03ilmcs.mongodb.net",
+    "port": 27017,
+    "database": "atividade-5"
+}
+
+credenciais = {
+    "user": "admin",
+    "password": "U76JsRTQ2vLeBl9O",
+}
